31e5b8f8117f99337a713c37e5039b6b2bc3c464
olivier.bensemhoun <olivier.bensemhoun@epita.fr>
2025-11-20 10:32:44 +0100

    build: set up file project

---
+++ ProxiBanque_Olivier_Bensemhoun/src/test/java/com/example/proxibanque_olivier_bensemhoun/ProxiBanqueOlivierBensemhounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxibanque_olivier_bensemhoun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueOlivierBensemhounApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
